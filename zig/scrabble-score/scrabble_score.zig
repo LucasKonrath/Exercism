df180cfa67c6bacf7b2a71172e8915ed95ff6445
LucasKonrath <lucaskdamaceno@gmail.com>
2025-06-09 22:08:50 -0700

    Add Zig scrabble-score exercise

---
+++ zig/scrabble-score/scrabble_score.zig
@@ -0,0 +1,19 @@
+const ascii = @import("std").ascii;
+
+pub fn score(s: []const u8) u32 {
+    var totalScore: u32 = 0;
+
+    for(s) |letter| {
+        totalScore += switch(ascii.toUpper(letter)){
+            'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' => 1,
+            'D', 'G' => 2,
+            'B', 'C', 'M', 'P' => 3,
+            'F', 'H', 'V', 'W', 'Y'=> 4,
+            'K' => 5,
+            'J', 'X' => 8,
+            'Q', 'Z' => 10,
+            else => 0
+        };
+    }
+    return totalScore;
+}
