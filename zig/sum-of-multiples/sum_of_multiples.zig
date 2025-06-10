e20d06a9190ce06cb7c35ba39a977405099d838d
LucasKonrath <lucaskdamaceno@gmail.com>
2025-06-10 07:29:19 -0700

    Implement Zig sum-of-multiples solution

--- zig/sum-of-multiples/sum_of_multiples.zig
+++ zig/sum-of-multiples/sum_of_multiples.zig
@@ -2,8 +2,22 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn sum(allocator: mem.Allocator, factors: []const u32, limit: u32) !u64 {
-    _ = allocator;
-    _ = factors;
-    _ = limit;
-    @compileError("please implement the sum function");
+
+    // declare set for unique mappings
+    var set = try std.DynamicBitSet.initEmpty(allocator, limit + 1);
+    defer set.deinit();
+
+    for(factors) | factor | if(factor > 0) {
+        var multiple = factor;
+        while (multiple < limit) : (multiple += factor) set.set(multiple);
+    };
+
+    var sumFactors: u64 = 0;
+
+    var iter = set.iterator(.{});
+    while(iter.next()) |value| {
+        sumFactors += value;
+    }
+
+    return sumFactors;
 }
