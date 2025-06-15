4f49dea5bba64ac9cc211e9d37ea3ceea0b84d9a
LucasKonrath <lucaskdamaceno@gmail.com>
2025-06-15 19:46:46 -0300

    Add ISBN Verifier in Zig

--- zig/isbn-verifier/isbn_verifier.zig
+++ zig/isbn-verifier/isbn_verifier.zig
@@ -1,4 +1,28 @@
 pub fn isValidIsbn10(s: []const u8) bool {
-    _ = s;
-    @compileError("please implement the isValidIsbn10 function");
+    var digitsUsed: u8 = 0;
+    var sum: u32 = 0;
+
+    for(s) |digit|{
+        if(digitsUsed == 10){
+            return false;
+        }
+
+        if(digit == '-'){
+            continue;
+        }
+
+        if(digit == 'X' and digitsUsed == 9){
+            sum += 10;
+            digitsUsed += 1;
+            continue;
+        }
+
+        if(digit >= '0' and digit <= '9'){
+            sum += (digit - '0') * (10 - digitsUsed);
+            digitsUsed += 1;
+            continue;
+        }
+        return false;
+    }
+    return digitsUsed == 10 and sum % 11 == 0;
 }
