67b9b600c8bd8fec3d9358ab493732e44480c246
LucasKonrath <lucaskdamaceno@gmail.com>
2025-06-15 19:38:36 -0300

    Refactor rotational cipher to use switch

--- zig/rotational-cipher/rotational_cipher.zig
+++ zig/rotational-cipher/rotational_cipher.zig
@@ -4,15 +4,12 @@ const mem = std.mem;
 pub fn rotate(allocator: mem.Allocator, text: []const u8, shiftKey: u5) mem.Allocator.Error![]u8 {
     const encrypted = try allocator.alloc(u8, text.len);
 
-    for(0..text.len) |curr| {
-        const currChar = text[curr];
-        if(currChar >= 'A' and currChar <= 'Z'){
-            encrypted[curr] = (currChar - 'A' + shiftKey) % 26 + 'A';
-        } else if (currChar >= 'a' and currChar <= 'z'){
-            encrypted[curr] = (currChar - 'a' + shiftKey) % 26 + 'a';
-        } else {
-            encrypted[curr] = currChar;
-        }
+    for(text, 0..) |curr, i| {
+        encrypted[i] = switch (curr) {
+            'A'...'Z' => (curr - 'A' + shiftKey) % 26 + 'A',
+            'a'...'z' => (curr - 'a' + shiftKey) % 26 + 'a',
+            else => curr
+        };
     }
 
     return encrypted;
