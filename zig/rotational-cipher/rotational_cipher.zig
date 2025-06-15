47bb191aa137642a865707fdcc367335daa22f58
LucasKonrath <lucaskdamaceno@gmail.com>
2025-06-15 19:35:09 -0300

    Finish rotational cipher

--- zig/rotational-cipher/rotational_cipher.zig
+++ zig/rotational-cipher/rotational_cipher.zig
@@ -2,8 +2,18 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn rotate(allocator: mem.Allocator, text: []const u8, shiftKey: u5) mem.Allocator.Error![]u8 {
-    _ = allocator;
-    _ = text;
-    _ = shiftKey;
-    @compileError("please implement the rotate function");
+    const encrypted = try allocator.alloc(u8, text.len);
+
+    for(0..text.len) |curr| {
+        const currChar = text[curr];
+        if(currChar >= 'A' and currChar <= 'Z'){
+            encrypted[curr] = (currChar - 'A' + shiftKey) % 26 + 'A';
+        } else if (currChar >= 'a' and currChar <= 'z'){
+            encrypted[curr] = (currChar - 'a' + shiftKey) % 26 + 'a';
+        } else {
+            encrypted[curr] = currChar;
+        }
+    }
+
+    return encrypted;
 }
