709056ff506398e3e4e3f087248b9a84be7ca840
LucasKonrath <lucaskdamaceno@gmail.com>
2025-06-15 22:14:46 -0300

    Finish acronym in zig

--- zig/acronym/acronym.zig
+++ zig/acronym/acronym.zig
@@ -2,7 +2,10 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn abbreviate(allocator: mem.Allocator, words: []const u8) mem.Allocator.Error![]u8 {
-    _ = allocator;
-    _ = words;
-    @compileError("please implement the abbreviate function");
+    var acronym = std.ArrayList(u8).init(allocator);
+    var wordsTokenized = std.mem.tokenizeAny(u8, words, " _-");
+    while (wordsTokenized.next()) |word| {
+        try acronym.append(word[0] & '_');
+    }
+    return acronym.toOwnedSlice();
 }
