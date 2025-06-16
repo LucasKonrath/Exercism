1d133e21c03e50594e109434957805e2423e21f2
LucasKonrath <lucaskdamaceno@gmail.com>
2025-06-15 21:38:34 -0300

    Finish zig allergies

--- zig/allergies/allergies.zig
+++ zig/allergies/allergies.zig
@@ -1,24 +1,30 @@
 const std = @import("std");
 const EnumSet = std.EnumSet;
 
-pub const Allergen = enum {
-    eggs,
-    peanuts,
-    shellfish,
-    strawberries,
-    tomatoes,
-    chocolate,
-    pollen,
-    cats,
+pub const Allergen = enum(u8) {
+    eggs = 1,
+    peanuts = 2,
+    shellfish = 4,
+    strawberries = 8,
+    tomatoes = 16,
+    chocolate = 32,
+    pollen = 64,
+    cats = 128,
 };
 
 pub fn isAllergicTo(score: u8, allergen: Allergen) bool {
-    _ = score;
-    _ = allergen;
-    @compileError("please implement the isAllergicTo function");
+    return @intFromEnum(allergen) & score  > 0;
 }
 
 pub fn initAllergenSet(score: usize) EnumSet(Allergen) {
-    _ = score;
-    @compileError("please implement the initAllergenSet function");
+    var set = EnumSet(Allergen) {};
+    // lower from usize to u8
+    const score_u8: u8 = @truncate(score);
+
+    for(std.enums.values(Allergen)) |allergen| {
+        if(isAllergicTo(score_u8, allergen)){
+            set.insert(allergen);
+        }
+    }
+    return set;
 }
