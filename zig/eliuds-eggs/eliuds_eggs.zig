45359f856b4d2946e0a719a0ac7ec9983531953f
LucasKonrath <lucaskdamaceno@gmail.com>
2025-06-15 22:05:37 -0300

    Finish eliud's eggs with popCount

--- zig/eliuds-eggs/eliuds_eggs.zig
+++ zig/eliuds-eggs/eliuds_eggs.zig
@@ -1,4 +1,4 @@
 pub fn eggCount(number: usize) usize {
-    _ = number;
-    @compileError("please implement the eggCount function");
+    // https://ziglang-org.translate.goog/documentation/master/?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt&_x_tr_pto=tc#toc-popCount
+    return @popCount(number);
 }
