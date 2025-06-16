cd03717dc40200e33bf8d6c88e10034c7caf8790
LucasKonrath <lucaskdamaceno@gmail.com>
2025-06-15 22:28:38 -0300

    Finish sieve of eratosthenes

--- zig/sieve/sieve.zig
+++ zig/sieve/sieve.zig
@@ -1,5 +1,22 @@
-pub fn primes(buffer: []u32, limit: u32) []u32 {
-    _ = buffer;
-    _ = limit;
-    @compileError("please implement the primes function");
+const std = @import("std");
+
+pub fn primes(buffer: []u32, comptime limit: u32) []u32 {
+    var sieve = std.StaticBitSet(limit + 1).initEmpty();
+    sieve.set(0);
+    sieve.set(1);
+
+    var index: u32 = 0;
+    var currentNumber: u32 = 2;
+
+    while(currentNumber <= limit) : (currentNumber += 1){
+        if(!sieve.isSet(currentNumber)){
+            buffer[index] = currentNumber;
+            index += 1;
+            var squared = currentNumber * currentNumber;
+            while(squared <= limit) : (squared += currentNumber) {
+                sieve.set(squared);
+            }
+        }
+    }
+    return buffer[0..index];
 }
