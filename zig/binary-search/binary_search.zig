2ed6f5c1bc7a04e319c347856adc30bef46eab0b
LucasKonrath <lucaskdamaceno@gmail.com>
2025-06-15 21:46:25 -0300

    Finish zig binary search

--- zig/binary-search/binary_search.zig
+++ zig/binary-search/binary_search.zig
@@ -1,7 +1,22 @@
 // Take a look at the tests, you might have to change the function arguments
+pub fn binarySearch(comptime T: type, target: usize, items: []const T) ?usize {
 
-pub fn binarySearch(target: usize, items: []const usize) ?usize {
-    _ = target;
-    _ = items;
-    @compileError("please implement the binarySearch function");
+    var leftPointer: usize = 0;
+    var rightPointer: usize = items.len;
+
+    while(leftPointer < rightPointer){
+
+        const mid = (leftPointer + rightPointer) / 2;
+
+        if(items[mid] < target){
+            leftPointer = mid + 1;
+        } else if (target < items[mid]){
+            rightPointer = mid;
+        } else if (items[mid] == target){
+            return mid;
+        }
+    }
+
+    return null;
+
 }
