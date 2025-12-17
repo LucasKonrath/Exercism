a9e38f23f60d999e97202ac85a312f67aaacf5c1
LucasKonrath <lucaskdamaceno@gmail.com>
2025-12-17 16:23:19 -0300

    solve squaky clean

--- julia/squeaky-clean/squeaky-clean.jl
+++ julia/squeaky-clean/squeaky-clean.jl
@@ -1,7 +1,19 @@
 function transform(ch)
-
+    if(ch == '-')
+        return "_"
+    elseif isspace(ch)
+        return ""
+    elseif isuppercase(ch)
+        return "-" * lowercase(ch)
+    elseif isdigit(ch)
+        return ""
+    elseif 'α' <= ch <= 'ω'
+        return "?"
+    else
+        return string(ch)
+    end
 end
 
 function clean(str)
-
+    return join(transform(ch) for ch in str)
 end
